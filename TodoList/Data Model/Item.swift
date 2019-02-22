f4598ca518fb6456299a10706f640a3b047ce8c3
Darrieumerlou Simon <simon-23@hotmail.fr>
2019-02-22 14:34:30 +0100

    Convert to MVC design pattern

---
+++ TodoList/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  TodoList
+//
+//  Created by Darrieumerlou on 22/02/2019.
+//  Copyright © 2019 Darrieumerlou. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
